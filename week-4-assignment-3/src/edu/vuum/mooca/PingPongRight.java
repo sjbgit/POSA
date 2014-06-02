62bb43287db987160980f1620dd83ddb2d4ce678
sjbgit <sjb_strat@hotmail.com>
2014-06-02 00:25:58 -0500

    cleaned up

--- week-4-assignment-3/src/edu/vuum/mooca/PingPongRight.java
+++ week-4-assignment-3/src/edu/vuum/mooca/PingPongRight.java
@@ -52,7 +52,7 @@ public class PingPongRight {
                                   SimpleSemaphore semaphoreOne,
                                   SimpleSemaphore semaphoreTwo,
                                   int maxIterations) {
-            // TODO - You fill in here. tet
+            // TODO - You fill in here. 
         }
 
         /**
